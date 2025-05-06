1613b2205ee0e1db9346b3ad7d2a5ed53f03d419
BV <bastien.villefort@icloud.com>
2025-05-06 13:36:42 +0300

    first commit

---
+++ SymptomlyTests/SymptomlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SymptomlyTests.swift
+//  SymptomlyTests
+//
+//  Created by Bastien Villefort on 5/6/25.
+//
+
+import Testing
+@testable import Symptomly
+
+struct SymptomlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
